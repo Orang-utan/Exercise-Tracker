8c82d936bc830cf448e3959e0aec48cb74ae78a5
Daniel Tian <dtian.95@gmail.com>
2019-08-15 16:08:45 +0800

    Ready for production

---
+++ client/build/precache-manifest.8300f2ee17fbd7e8a84d92646cad5974.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8346294c8b44b00779980483c9cbbac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de12b071a2939d57e994",
+    "url": "/static/css/2.a73d7da8.chunk.css"
+  },
+  {
+    "revision": "75246f798872b85911c0",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "de12b071a2939d57e994",
+    "url": "/static/js/2.471249c0.chunk.js"
+  },
+  {
+    "revision": "75246f798872b85911c0",
+    "url": "/static/js/main.17e05b9c.chunk.js"
+  },
+  {
+    "revision": "ff845537ca6036709bc3",
+    "url": "/static/js/runtime~main.cbb9c3ae.js"
+  }
+]);
---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const path = require("path");
+
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+///////////////////////////////////////////////////////////////
+// Connect to MongoDB
+///////////////////////////////////////////////////////////////
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connection established succesfully");
+});
+
+///////////////////////////////////////////////////////////////
+// Create Routes for CRUD operations
+///////////////////////////////////////////////////////////////
+const exercisesRouter = require("./routes/exercises");
+const usersRouter = require("./routes/users");
+
+app.use("/exercises", exercisesRouter);
+app.use("/users", usersRouter);
+
+///////////////////////////////////////////////////////////////
+// Ready for Production
+///////////////////////////////////////////////////////////////
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (request, response) => {
+    response.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
+
+///////////////////////////////////////////////////////////////
+// Testing Function (Runs after Start)
+///////////////////////////////////////////////////////////////
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
