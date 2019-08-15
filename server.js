85244a4f884618d2408dd40704e163dcb9c1643f
Daniel Tian <dtian.95@gmail.com>
2019-08-15 21:41:41 +0800

    Debug

--- server.js
+++ server.js
@@ -36,9 +36,9 @@ app.use("/users", usersRouter);
 ///////////////////////////////////////////////////////////////
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  app.get("*", (request, response) => {
-    response.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  });
+  // app.get("*", (request, response) => {
+  //   response.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  // });
 }
 
 ///////////////////////////////////////////////////////////////
